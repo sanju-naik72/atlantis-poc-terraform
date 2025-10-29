b27d32738e6d1ed163f2ec516874577f08ed6d64
Sanju Naik <sanjunaik@Sanjus-MacBook-Air.local>
2025-10-29 07:40:48 +0530

    test-atlantis

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "172.0.0.0/16"
 }
 
 variable "environment" {
